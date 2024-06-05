c1fac24a13228d87847a54014d104634c29c4215
Me-k-01 <mecaman01@gmail.com>
2024-06-05 23:35:19 +0200

    Work in progress for the sphere triangulation.

--- scripts/sphere.js
+++ scripts/sphere.js
@@ -9,8 +9,9 @@
 
 function addGizmo(pos, vertices, indices, colors, size = 0.01) {  
     const offsetId = vertices.length / 3; // Nombre actuel de sommets / 3 (car chaque sommet a 3 coordonnées)
-    
-    const newVertices = [
+     
+    // Appliquer la transformation et la position
+    const transformedVertices = [
         // Front face
         -1, -1, 1,      1, -1, 1,      1, 1, 1,     -1, 1, 1,
         // Back face
@@ -23,10 +24,7 @@ function addGizmo(pos, vertices, indices, colors, size = 0.01) {
         1, -1, -1,      1, 1, -1,      1, 1, 1,      1, -1, 1,
         // Left face
         -1, -1, -1,    -1, -1, 1,     -1, 1, 1,     -1, 1, -1,
-    ];
-
-    // Appliquer la transformation et la position
-    const transformedVertices = newVertices.map((p, i) => p * size + pos.get(i % 3));
+    ].map((p, i) => p * size + pos.get(i % 3));
     vertices.push(...transformedVertices);
  
     // Mise à jour des indices en tenant compte de la base
@@ -50,11 +48,13 @@ function addGizmo(pos, vertices, indices, colors, size = 0.01) {
     ];  
     for (const c of faceCols) { 
       // Four vertices of the face 
-      colors.push(...c) ;
-      colors.push(...c) ;
-      colors.push(...c) ;
-      colors.push(...c) ;
+      colors.push(...c, ...c, ...c, ...c) ;  
     }   
+}
+
+// Returns the middle of the triangle
+function middle(v0, v1, v2) {
+    return Vec3.add(v0, v1).add(v2);
 }
 
 /** Sphere **/
@@ -81,24 +81,114 @@ class Sphere {
          */
         this.n = n ; 
     } 
+    
+    /**
+     * Triangulisation de delauney contraintes 
+     */
+    delauney() {
+        
+    }
 
-    fibonacciSphere(samples) {   
+    
+    /**
+     * Generation d'une sphere de fibonacci
+     * @param {number} nVertices nombre de points sur la sphere
+     */
+    fibonacciSphere(nVertices) {   
         let vertices = [];
         let indices = [];
         let colors = [];
 
-        const phi = Math.PI * (Math.sqrt(5) - 1);  // golden angle in radians  
-        for (let i = 0; i < samples; i++) {
-            const y = 1 - (i / samples) * 2;  // y goes from 1 to -1
+        let mesh = []
+
+        const scale = 2; // Scale of the sphere 
+        const phi = Math.PI * (Math.sqrt(5) - 1);  // golden angle in radians
+
+        for (let i = 0; i < nVertices; i++) {
+            const y = (1 - (i / nVertices) * 2);  // y goes from 1 to -1
             const radius = Math.sqrt(1 - y * y);  // radius at y
     
             const theta = phi * i;  // golden angle increment
     
             const x = Math.cos(theta) * radius;
-            const z = Math.sin(theta) * radius;   
-            addGizmo(new Vec3(x, y, z), vertices, indices, colors);  
+            const z = Math.sin(theta) * radius;  
+            
+            const pos = new Vec3(x, y, z).mul(scale); 
+
+            vertices.push(pos.x, pos.y, pos.z); 
+            colors.push(Math.random(), Math.random(), 1.0, 1.0); 
+
+            mesh.push(pos); 
         }
-         
+        
+        // Pour chaque potentiel triangle de la sphere 
+        const sliceMesh = Math.trunc(nVertices/3); 
+        for (let i = 0; i < sliceMesh ; i++) {
+            for (let j = sliceMesh; j < sliceMesh*2; j++) {
+
+            current_triangle_to_evaluate:
+                for (let k = sliceMesh*2; k < nVertices ; k++) {
+                    // On forme le triangle à évaluer  
+                    // Le plan est définis selon la normal 
+                    // console.log(mesh[i]);
+                    // console.log(mesh[j]);
+                    // console.log(mesh[k]);
+                    // console.log(Vec3.sub(mesh[j], mesh[i]));
+                    // console.log(Vec3.sub(mesh[k], mesh[i]));
+                    let normal = Vec3.sub(mesh[j], mesh[i]).cross(Vec3.sub(mesh[k], mesh[i])) ;
+                    normal.normalize();
+
+                    const m = middle(mesh[i], mesh[j], mesh[k])
+                    // On arange les points pour que leurs normal pointent vers l'exterieur de la sphere
+                    if (normal.dot( m ) < 0) { 
+                        // Inversion du sens de lecture des points du triangles   
+                        // const interm = j;
+                        // j = k;
+                        // k = interm;
+                        // normal.mul(-1);  
+                    }
+
+                    let sign = 0;
+                    // Verifie que le triangle est minimal, en s'assurant que chaque points est de l'autre coté du plan. 
+                    const polygones = [i, j, k]; // Points appartenant au plan
+                    for (let x = 0; x<nVertices; x++) {
+                        if (x === i || x === j || x === k) 
+                            continue;
+                        // Verifier qu'aucun point ne se trouve au dessus de la normal
+                        const cosTheta = normal.dot(Vec3.sub(mesh[x], mesh[i]));
+
+                        if (cosTheta === 0) {
+                            polygones.push(x);
+                            console.log("Same plane : ", polygones);
+                            continue;
+                        } 
+
+                        if (sign === 0) 
+                            sign = Math.sign(cosTheta); // Pick a sign for the first time
+                        if (sign !== Math.sign(cosTheta))
+                            break current_triangle_to_evaluate;
+
+                        // console.log(cosTheta);
+                        // if (cosTheta > 0) 
+                        //     break current_triangle_to_evaluate;
+                        // Cas ou le point est sur le plan (un cube circonscrie à la sphere possèderais des faces à 4 sommets par exemple)
+                         
+                    }
+                    // console.log("Adding triangle : ", polygones);
+                    // Ajouter ce triangles au rendu
+                    if (polygones.length === 3) {
+                        indices.push(i, j, k);
+                        indices.push(i, k, j);
+                    }
+                }
+            }
+        }
+
+        for (const pos of mesh) {
+            addGizmo(pos, vertices, indices, colors)
+        }
+        // console.log(indices);
+
         return {vertices, indices, colors};
     }
     
@@ -163,8 +253,7 @@ class Sphere {
             3, // Number of elements per attribute
             gl.FLOAT, // Type of elements
             false, // Normalize?
-            // 3 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
-            0,
+            0, // 3 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex 
             0 // Offset from the beginning of a single vertex to this attribute
         ); 
         gl.enableVertexAttribArray(this.positionAttribLocation);   
